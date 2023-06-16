b46b275f1573b357874b026aa983f255dcad67cf
Rafal <rafal.z.malucha@gmail.com>
2023-06-16 23:57:01 +0200

    add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Products from './components/Products.js'
+import Navbar from "./components/Navbar.js"
+import Popular from './components/Popular.js';
 
 class App extends React.Component {
 
 render() {
     return(
         <div>
+            <Navbar />
+            <Popular />
             <Products />
         </div>
     )
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+        return(
+            <div>
+                <div class="navbar bg-neutral">
+                    <a class="btn btn-ghost normal-case text-xl text-primary">daisyUI</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Navbar;
